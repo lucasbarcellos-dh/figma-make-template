6d28ad416c66fa631162b90911028a2c00663ba4
lucasbarcellos <lucas.araujo@deliveryhero.com>
2025-09-11 13:44:32 +0200

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './figma-make-dist/styles/globals.css'
-import App from './figma-make-dist/App.tsx'
+import './cape-template/styles/globals.css'
+import App from './cape-template/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

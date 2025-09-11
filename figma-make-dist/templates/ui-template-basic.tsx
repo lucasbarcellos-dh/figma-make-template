df31f9d7931d79eb59ecc7ce726bc4b3717af084
lucasbarcellos <lucas.araujo@deliveryhero.com>
2025-09-11 12:54:56 +0200

    Create ui-template-basic.tsx

---
+++ figma-make-dist/templates/ui-template-basic.tsx
@@ -0,0 +1,55 @@
+import SidenavPartnerPortal from "../patterns/sidenav-partner-portal";
+import HeaderPartnerPortal from "../patterns/header-partner-portal";
+
+interface UITemplateBasicProps {
+  title?: string;
+  userName?: string;
+  userImage?: string;
+  isOpen?: boolean;
+  onOpenClick?: () => void;
+  onHelpClick?: () => void;
+  onNotificationClick?: () => void;
+  onProfileClick?: () => void;
+  children?: React.ReactNode;
+}
+
+export default function UITemplateBasic({
+  title = "Page title",
+  userName = "User",
+  userImage = "https://github.com/shadcn.png",
+  isOpen = true,
+  onOpenClick,
+  onHelpClick,
+  onNotificationClick,
+  onProfileClick,
+  children
+}: UITemplateBasicProps) {
+  return (
+    <div className="flex h-screen w-full bg-background">
+      {/* Left Sidebar */}
+      <div className="max-w-[296px] w-[296px] flex-shrink-0">
+        <SidenavPartnerPortal />
+      </div>
+      
+      {/* Right Content Area */}
+      <div className="flex-1 flex flex-col">
+        {/* Header */}
+        <HeaderPartnerPortal
+          title={title}
+          userName={userName}
+          userImage={userImage}
+          isOpen={isOpen}
+          onOpenClick={onOpenClick}
+          onHelpClick={onHelpClick}
+          onNotificationClick={onNotificationClick}
+          onProfileClick={onProfileClick}
+        />
+        
+        {/* Main Content */}
+        <main className="flex-1 overflow-auto">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
